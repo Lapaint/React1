b470098ed87520429708cd25e8bcf6a409452fda
Lapaint <rlaalsdn0321@gmail.com>
2026-04-08 11:27:27 +0900

    2026.04.08

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,6 +1,20 @@
+// export default function Item({ name, isPacked }) {
+//     if (isPacked) {
+//     return <li>{name} ✅</li>;
+//   }
+//   return <li>{name}</li>;
+// }
+
 export default function Item({ name, isPacked }) {
-    if (isPacked) {
-    return <li>{name} ✅</li>;
-  }
-  return <li>{name}</li>;
+  return (
+    <li>
+      {isPacked ? (
+        <del>
+          {name + ' ✅'}
+        </del>
+      ) : (
+        name
+      )}
+    </li>
+  );
 }
